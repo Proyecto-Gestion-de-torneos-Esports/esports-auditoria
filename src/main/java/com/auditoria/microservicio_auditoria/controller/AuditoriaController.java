84bd55cb98ff367508dba953a734eea50e240618
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-05-10 18:00:10 -0400

    Implementación de manejo de excepciones global

--- src/main/java/com/auditoria/microservicio_auditoria/controller/AuditoriaController.java
+++ src/main/java/com/auditoria/microservicio_auditoria/controller/AuditoriaController.java
@@ -3,10 +3,8 @@ package com.auditoria.microservicio_auditoria.controller;
 
 import com.auditoria.microservicio_auditoria.dto.AuditoriaRequestDTO;
 import com.auditoria.microservicio_auditoria.dto.AuditoriaResponseDTO;
-import com.auditoria.microservicio_auditoria.model.Auditoria;
 import com.auditoria.microservicio_auditoria.service.AuditoriaService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
